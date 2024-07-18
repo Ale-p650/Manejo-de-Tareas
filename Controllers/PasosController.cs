1eb27f1f9f1fdac1f22bc3d882b716d9c33f5f5b
Ale-p650 <ale.pavlinusich@gmail.com>
2024-07-18 15:11:11 -0300

    Funciones Agregar Pasos

---
+++ Controllers/PasosController.cs
@@ -0,0 +1,67 @@
+﻿using Manejo_de_Tareas.Entidades;
+using Manejo_de_Tareas.Models;
+using Manejo_de_Tareas.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manejo_de_Tareas.Controllers
+{
+    [Route("api/pasos")]
+    public class PasosController :ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly IServicioUsuarios _servicioUsuarios;
+
+        public PasosController(ApplicationDBContext context,
+                                IServicioUsuarios servicioUsuarios)
+        {
+            _context = context;
+            _servicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpPost("{tareaId:int}")]
+        public async Task<ActionResult<Paso>> Post(int tareaId, 
+            [FromBody] PasoCrearDTO pasoCrearDTO)
+        {
+            var usuarioId = _servicioUsuarios.obtenerUsuarioID();
+
+            var tarea = await _context.Tareas.FirstOrDefaultAsync(t => t.Id == tareaId);
+
+            if (tarea is null)
+            {
+                return NotFound();
+            }
+
+            if(tarea.UsuarioCreacionId != usuarioId)
+            {
+                return Forbid();
+            }
+
+            var existenPasos = await _context.Pasos.AnyAsync(p => p.TareaId == tarea.Id);
+
+            var ordenMayor = 0;
+
+            if (existenPasos)
+            {
+                ordenMayor = await _context.Pasos
+                .Where(p => p.TareaId == tarea.Id)
+                .Select(p => p.Orden)
+                .MaxAsync();
+            }
+
+            Paso paso = new()
+            {
+                TareaId = tareaId,
+                Orden = ordenMayor + 1,
+                Descripcion = pasoCrearDTO.Descripcion,
+                Realizado = pasoCrearDTO.Realizado
+            };
+
+            _context.Add(paso);
+            await _context.SaveChangesAsync();
+
+            return paso;
+
+        }
+    }
+}
---
+++ Models/PasoCrearDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Manejo_de_Tareas.Models
+{
+    public class PasoCrearDTO
+    {
+        public string Descripcion { get; set; }
+        public bool Realizado { get; set; }
+    }
+}
